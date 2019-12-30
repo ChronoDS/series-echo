371debca4b7ed4c2a769f440063e2637c21575a1
daniel.sh.ds@gmail.com <Ds302596>
2019-12-30 21:28:52 +0200

    updated packages

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Header = () => {
+
+    const handleDelete = (funcParameter1, funcParameter2) => {
+        // func body
+    };
+
+    return (
+        <div>
+            Header Series Echo B-Ach:
+        </div>
+    )
+};
---
+++ src/components/Movie.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Movie extends React.Component {
+
+    handleDelete = () => {};
+
+    render() {
+        return (
+            <div>
+                Movie Lord Of The Rings
+            </div>
+        )
+    }
+}
+
+export default Movie;
